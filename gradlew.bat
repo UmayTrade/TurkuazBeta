f394dc6bc63e636cc224948679f8fa2b59c10a56
UmayTrade <kocatepe828@gmail.com>
2025-09-29 16:37:28 +0300

    Add files via upload

--- gradlew.bat
+++ gradlew.bat
@@ -1,16 +1,8 @@
-@echo off
-REM -------------------------------------------------------------
-REM Gradle Wrapper for Windows
-REM Automatically runs the Gradle Wrapper JAR
-REM -------------------------------------------------------------
-
+﻿@echo off
 SET DIR=%~dp0
 SET GRADLE_WRAPPER_JAR=%DIR%gradle\wrapper\gradle-wrapper.jar
-
 IF NOT EXIST "%GRADLE_WRAPPER_JAR%" (
     ECHO Gradle wrapper JAR not found.
-    ECHO Please run 'gradle wrapper' locally to generate the wrapper files.
     EXIT /B 1
 )
-
 java -jar "%GRADLE_WRAPPER_JAR%" %*
---
+++ src/main/java/com/umaytrade/Main.java
@@ -0,0 +1,6 @@
+﻿package com.umaytrade;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(""UmayTv4 Build Test"");
+    }
+}
