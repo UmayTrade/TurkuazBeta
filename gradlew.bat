56d8fc39fca555bdbed39dd7d296d8317c72081e
UmayTrade <kocatepe828@gmail.com>
2025-09-29 16:19:28 +0300

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,12 +1,17 @@
 @echo off
+REM -------------------------------------------------------------
 REM Gradle Wrapper for Windows
+REM Automatically runs the Gradle Wrapper JAR
+REM -------------------------------------------------------------
 
 SET DIR=%~dp0
 SET GRADLE_WRAPPER_JAR=%DIR%gradle\wrapper\gradle-wrapper.jar
 
 IF NOT EXIST "%GRADLE_WRAPPER_JAR%" (
-    ECHO Gradle wrapper JAR not found. Run 'gradle wrapper' locally.
+    ECHO Gradle wrapper JAR not found.
+    ECHO Please run 'gradle wrapper' locally to generate the wrapper files.
     EXIT /B 1
 )
 
+REM Execute Java with the wrapper JAR and pass all arguments
 java -jar "%GRADLE_WRAPPER_JAR%" %*
